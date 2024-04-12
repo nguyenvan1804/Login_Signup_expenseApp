b91c5e7d7d74e72f9a1005c0350321f51aa6372c
Harry Nguyen <hainguyen27798@gmail.com>
2024-04-12 13:36:59 +0700

    login and signup

--- android/app/src/main/kotlin/com/example/login_signup_expense_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/login_signup_expense_app/MainActivity.kt
@@ -1,5 +1,9 @@
-package com.example.login_signup_expense_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+//package com.example.login_signup_expense_app
+//
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
+//
+//open class FlutterActivity {
+//
+//}
